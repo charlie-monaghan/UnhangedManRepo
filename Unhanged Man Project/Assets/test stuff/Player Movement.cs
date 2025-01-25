83498aa113698cfdfb8ec5473a0b32ef011e9e00
Charlie Monaghan <cvmcharlie@gmail.com>
2025-01-24 19:16:33 -0500

    added attack and improved character
    added "attack" feature that turns on a damage field when you click mouse 1, and made it so that the character faces the correct direction when moving left and right

--- Unhanged Man Project/Assets/test stuff/Player Movement.cs
+++ Unhanged Man Project/Assets/test stuff/Player Movement.cs
@@ -1,3 +1,5 @@
+//Created by: charlie
+//Edited by:
 using UnityEngine;
 using UnityEngine.Rendering;
 
@@ -5,8 +7,9 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
     [SerializeField] private float jumpForce;
-
+    private bool isRight = true;
     private Rigidbody2D rigidBody2D;
+
     void Start()
     {
         rigidBody2D = GetComponent<Rigidbody2D>();
@@ -14,13 +17,32 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
+        //movement
         float moveInput = Input.GetAxisRaw("Horizontal");
         transform.position += new Vector3(moveInput, 0, 0) * speed * Time.deltaTime;
 
+        //fliping sprite
+        if (moveInput > 0 && !isRight)
+        {
+            Vector3 currentScale = transform.localScale; //fetching current scale
+            currentScale.x *= -1; //inversing current scale
+            transform.localScale = currentScale; //reassigning current scale
+            isRight = !isRight; //toggle isRight
+        }
+        else if (moveInput < 0 && isRight)
+        {
+            Vector3 currentScale = transform.localScale;
+            currentScale.x *= -1;
+            transform.localScale = currentScale;
+            isRight = !isRight;
+        }
+
+        //jump button
         if (Input.GetKeyDown(KeyCode.Space) && Mathf.Abs(rigidBody2D.linearVelocity.y) < 0.001f) 
         {
             rigidBody2D.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
         }
-        
+
+
     }
 }
