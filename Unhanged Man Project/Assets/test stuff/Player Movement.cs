ab5da964e565d7208ed7f95a25dea8eb05518762
Edward Menello <edmenello@gmail.com>
2025-01-27 18:59:02 -0500

    Implemented Player dodge

--- Unhanged Man Project/Assets/test stuff/Player Movement.cs
+++ Unhanged Man Project/Assets/test stuff/Player Movement.cs
@@ -1,5 +1,6 @@
 //Created by: charlie
 //Edited by:
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Rendering;
 
@@ -7,42 +8,91 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
     [SerializeField] private float jumpForce;
+    [SerializeField] private float rollSpeed;
     private bool isRight = true;
     private Rigidbody2D rigidBody2D;
+    private enum State
+    {
+        Moving,
+        Rolling
+    }
+    private State state;
+
+    private bool isInvincible = false;
+
+    private Vector3 rollDirection;
 
     void Start()
     {
+        state = State.Moving;
         rigidBody2D = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        //movement
-        float moveInput = Input.GetAxisRaw("Horizontal");
-        transform.position += new Vector3(moveInput, 0, 0) * speed * Time.deltaTime;
+        switch (state) {
+        case State.Moving:
+            //movement
+            float moveInput = Input.GetAxisRaw("Horizontal");
+            transform.position += new Vector3(moveInput, 0, 0) * speed * Time.deltaTime;
 
-        //fliping sprite
-        if (moveInput > 0 && !isRight)
+            //fliping sprite
+            if (moveInput > 0 && !isRight)
+            {
+                Vector3 currentScale = transform.localScale; //fetching current scale
+                currentScale.x *= -1; //inversing current scale
+                transform.localScale = currentScale; //reassigning current scale
+                isRight = !isRight; //toggle isRight
+            }
+            else if (moveInput < 0 && isRight)
+            {
+                Vector3 currentScale = transform.localScale;
+                currentScale.x *= -1;
+                transform.localScale = currentScale;
+                isRight = !isRight;
+            }
+    
+            //jump button
+            if (Input.GetKeyDown(KeyCode.Space) && Mathf.Abs(rigidBody2D.linearVelocity.y) < 0.001f)
+            {
+                rigidBody2D.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            }
+            //Add a check for on the ground, right click
+            if (Input.GetKeyDown(KeyCode.Mouse1))
+            {
+                //rigidBody2D.AddForce(new Vector2(rollSpeed * moveInput, 0), ForceMode2D.Impulse);
+                rollSpeed = 0.5f;
+                state = State.Rolling;
+                rollDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, 0).normalized;
+                isInvincible = true;
+            }
+            break;
+
+        case State.Rolling:
+            float rollSpeedDropMultiplier = 5f;
+            rollSpeed -= rollSpeed * rollSpeedDropMultiplier * Time.deltaTime;
+            float rollSpeedMinimum = 0.1f;
+            if (rollSpeed < rollSpeedMinimum)
+            {
+                state = State.Moving;
+                isInvincible = false;
+            }
+            break;
+        }   
+    }
+    private void FixedUpdate()
+    {
+        switch (state)
         {
-            Vector3 currentScale = transform.localScale; //fetching current scale
-            currentScale.x *= -1; //inversing current scale
-            transform.localScale = currentScale; //reassigning current scale
-            isRight = !isRight; //toggle isRight
+            case State.Moving:
+                break;
+            case State.Rolling:
+                transform.position += rollDirection * rollSpeed;
+                break;
         }
-        else if (moveInput < 0 && isRight)
-        {
-            Vector3 currentScale = transform.localScale;
-            currentScale.x *= -1;
-            transform.localScale = currentScale;
-            isRight = !isRight;
-        }
-
-        //jump button
-        if (Input.GetKeyDown(KeyCode.Space) && Mathf.Abs(rigidBody2D.linearVelocity.y) < 0.001f) 
-        {
-            rigidBody2D.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
-        }
-
-
+    }
+    public bool IsInvincible()
+    {
+        return isInvincible;
     }
 }
