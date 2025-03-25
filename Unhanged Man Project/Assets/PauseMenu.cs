4eaa231166658e25658aa8af390f5553fa27f1ca
Edward Menello <edmenello@gmail.com>
2025-03-25 16:51:52 -0400

    Pause game works

--- Unhanged Man Project/Assets/PauseMenu.cs
+++ Unhanged Man Project/Assets/PauseMenu.cs
@@ -2,18 +2,36 @@ using UnityEngine;
 
 public class PauseMenu : MonoBehaviour
 {
-    
+    [SerializeField] GameObject pauseMenu;
     void Start()
     {
-        Time.timeScale = 0f;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (pauseMenu.activeSelf == true)
         {
-            Time.timeScale = 1f;
+            PauseGame();
         }
+        if (Input.GetKeyDown(KeyCode.Escape) && pauseMenu.activeSelf == true)
+        {
+            ResumeGame();
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape) && pauseMenu.activeSelf == false)
+        {
+            PauseGame();
+        }
+    }
+    public void ResumeGame()
+    {
+        Time.timeScale = 1f;
+        pauseMenu.SetActive(false);
+    }
+    public void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
     }
 }
