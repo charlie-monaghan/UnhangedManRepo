1a391a35ec84ee970264382f577a9a93fff25b2b
Charlie Monaghan <cvmcharlie@gmail.com>
2025-03-25 16:56:09 -0400

    changes to health and letter box script
    added another event to the health script and fleshed the boss letter box script out a little bit, still need to add the logic to actually draw the letter inside the box

--- Unhanged Man Project/Assets/Scripts/Boss Letter Box.cs
+++ Unhanged Man Project/Assets/Scripts/Boss Letter Box.cs
@@ -4,12 +4,38 @@ public class BossLetterBox : MonoBehaviour
 {
     [SerializeField] GameObject bossLetter;
 
+    Health bossEnemyHealth;
+    SpriteRenderer spriteRenderer;
+    Sprite bossLetterSprite;
+
     void Start()
+    {
+        bossEnemyHealth = bossLetter.GetComponent<Health>();
+        spriteRenderer = bossLetter.GetComponent<SpriteRenderer>();
+        Sprite bossLetterSprite = spriteRenderer.sprite;
+
+        if(bossEnemyHealth != null)
+        {
+            bossEnemyHealth.onBossEnemyDeath += DrawDeadEnemy;
+            DrawDeadEnemy();
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (bossEnemyHealth != null)
+        {
+            bossEnemyHealth.onBossEnemyDeath -= DrawDeadEnemy;
+            
+        }
+    }
+
+    void Update()
     {
         
     }
 
-    void Update()
+    private void DrawDeadEnemy()
     {
         
     }
