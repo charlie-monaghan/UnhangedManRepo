232e9968f7ebf4bcb8ee3a01e8fe3d13775f25fc
jazdlol <jazlyndailey03@gmail.com>
2025-03-25 15:18:01 -0400

    Volume control and settings page
    Implemented volume control and settings page that can interact with other pages. If you click the sfx button tests out the sfx volume and mixer

---
+++ Unhanged Man Project/Assets/Scripts/VolumeController.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class VolumeController : MonoBehaviour
+    {
+    [SerializeField] private AudioMixer audioMixer;
+    [SerializeField] private Slider musicSlider;
+    [SerializeField] private Slider sfxSlider;
+
+    void Start()
+    {
+        float savedMusicVolume = PlayerPrefs.GetFloat("MusicVolume", 0.75f); // saved volume settings
+        float savedSFXVolume = PlayerPrefs.GetFloat("SFXVolume", 0.75f); // saved volume settings
+        musicSlider.value = savedMusicVolume;
+        sfxSlider.value = savedSFXVolume;
+        SetMusicVolume(savedMusicVolume);
+        SetSFXVolume(savedSFXVolume);
+
+        // listener for when slider changes volume value
+        musicSlider.onValueChanged.AddListener(SetMusicVolume);
+        sfxSlider.onValueChanged.AddListener(SetSFXVolume);
+
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        // linear value converted to log value for audio mixer
+        float volumeDb = Mathf.Log10(Mathf.Clamp(volume, 0.0001f, 1f)) * 20;
+        audioMixer.SetFloat("MusicVolume", volumeDb);
+
+        //  new volume value is saved
+        PlayerPrefs.SetFloat("MusicVolume", volume);
+        PlayerPrefs.Save();
+    }
+
+    public void SetSFXVolume(float volume)
+    {
+        // linear value converted to log value for audio mixer
+        float volumeDb = Mathf.Log10(Mathf.Clamp(volume, 0.0001f, 1f)) * 20;
+        audioMixer.SetFloat("SFXVolume", volumeDb);
+
+        //  new volume value is saved
+        PlayerPrefs.SetFloat("SFXVolume", volume);
+        PlayerPrefs.Save();
+    }
+}
