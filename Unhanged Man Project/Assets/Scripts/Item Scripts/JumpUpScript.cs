018011e187c7d5682b61712694f6bdbb67b4e589
Charlie Monaghan <cvmcharlie@gmail.com>
2025-04-02 13:49:17 -0400

    jump up item and prefab folder organization
    added item that increases jump height, and organized the prefab folder a little bit more

---
+++ Unhanged Man Project/Assets/Scripts/Item Scripts/JumpUpScript.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class JumpUpScript : ItemInstance
+{
+    private void OnEnable()
+    {
+        itemSO.onItemPickup += OnItemPickup;
+    }
+
+    private void OnDisable()
+    {
+        itemSO.onItemPickup -= OnItemPickup;
+    }
+
+    private void OnItemPickup(string itemName)
+    {
+        if (itemSO.GetItemName() == itemName)
+        {
+            ItemEffect();
+        }
+    }
+
+    public override void ItemEffect()
+    {
+        base.ItemEffect();
+        Debug.Log("" + itemSO.stackAmount);
+        PlayerMovement playerMovement = itemSO.GetPlayer().GetComponent<PlayerMovement>();
+        playerMovement.jumpForce = 12f + (0.5f * itemSO.stackAmount);
+    }
+}
