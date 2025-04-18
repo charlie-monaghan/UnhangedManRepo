45db700ebcf4bd235858bf162585bc41eb2775f2
Carter Braun <carterbraun79@gmail.com>
2025-04-17 22:26:28 -0400

    Adjusted level 6, made hp pickups random

--- Unhanged Man Project/Assets/Scripts/Health Pickup.cs
+++ Unhanged Man Project/Assets/Scripts/Health Pickup.cs
@@ -1,8 +1,24 @@
 using UnityEngine;
+using System;
 
 public class HealthPickup : MonoBehaviour
 {
     [SerializeField] private int healthToHeal;
+    [SerializeField] private bool guaranteedToSpawn;
+    GameObject player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        if (!guaranteedToSpawn)
+        {
+            if (UnityEngine.Random.Range(0f, 10f) * Math.Sqrt(player.GetComponent<Health>().currentHealth / player.GetComponent<Health>().maxHealth) >= 3f)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
